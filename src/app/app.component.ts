18c7dece91cfe64512ff36489d856e04662f1e34
Miller <millershaw61@gmail.com>
2024-11-26 21:38:11 +0000

    Added count inc/clear

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IncrementService } from './increment.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-demo';
+  count = 0;
+
+  constructor(private inc: IncrementService) {}
+
+  ngOnInit(): void {
+    this.inc.count$.subscribe((newCount) => {
+      this.count = newCount;
+    });
+  }
+
+  incValue(): void {
+    this.inc.incrementValue();
+  }
+
+  clearVal(): void {
+    this.inc.clearCount();
+  }
 }
---
+++ src/app/increment.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IncrementService {
+  count = new BehaviorSubject<number>(0);
+  count$ = this.count.asObservable();
+
+  incrementValue() {
+    const curr = this.count.value;
+    this.count.next(curr + 1);
+  }
+
+  clearCount() {
+    this.count.next(0);
+  }
+}
